decf2035cd70f1592ec999944d28439dceee2733
Juan Holgado <juanholgado14@gmail.com>
2026-05-25 18:22:07 -0300

    Actualizo clase cliente

--- src/delivery/Cliente.java
+++ src/delivery/Cliente.java
@@ -1,5 +1,10 @@
 package delivery;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Cliente {
 
+	private final String nombre;
+	
+	private static final AtomicInteger contadorId = new AtomicInteger();
 }
